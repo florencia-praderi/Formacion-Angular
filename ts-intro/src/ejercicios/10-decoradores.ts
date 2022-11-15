e1bcd0bdcdacdd88e874053ed4274b4642f410b5
Florencia Praderi <flopraderi@gmail.com>
2022-11-15 11:47:11 +0100

    Seccion 3 - Ejercicios 10 y 11

---
+++ ts-intro/src/ejercicios/10-decoradores.ts
@@ -0,0 +1,13 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+class MiSuperClase{
+    public iPropiedad: string = 'ABC123';
+
+    imprimir(){
+        console.log('Hola Mundo')
+    }
+}
+
+console.log(MiSuperClase)
