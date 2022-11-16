70461c77c5d4f8791ca228aeb9176a284630326b
Florencia Praderi <flopraderi@gmail.com>
2022-11-16 16:46:15 +0100

    Seccion 4 - Fin
    Componentes, directivas, modulos

---
+++ Angular/01-bases/src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'app-listado',
+    templateUrl: 'listado.component.html'
+})
+export class ListadoComponent{
+    heroes: string[] = ['Spiderman', 'Ironman', 'Hulk', 'Batman', 'Thor'];
+    heroeBorrado: string = '';
+
+    borrarHeroe(){
+    const heroeBorrado = this.heroes.shift();
+    this.heroeBorrado = heroeBorrado || '';
+    }
+}
