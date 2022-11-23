edbbdd26fc8a04c077197ccf239404954d7f0455
Florencia Praderi <flopraderi@gmail.com>
2022-11-23 18:50:36 +0100

    Seccion 8 - Paises App

---
+++ Angular/03-paisesApp/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PaisModule } from './pais/pais.module';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PaisModule,
+    SharedModule
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
