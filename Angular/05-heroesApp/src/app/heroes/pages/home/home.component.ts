2c9e209a33d91dda4a61d734a8cc5eed1fc6a369
Florencia Praderi <flopraderi@gmail.com>
2022-12-02 15:07:15 +0100

    Seccion 13 - HeroesApp

--- Angular/05-heroesApp/src/app/heroes/pages/home/home.component.ts
+++ Angular/05-heroesApp/src/app/heroes/pages/home/home.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styles: [`.container{
+            margin: 10px;}
+          `]
 })
 export class HomeComponent implements OnInit {
 
