43f7f7d8421b12e8917cff4c83032ac980696bd4
Florencia Praderi <flopraderi@gmail.com>
2022-12-07 12:37:07 +0100

    Seccion 13 - Fin
    HeroesApp

--- Angular/05-heroesApp/src/app/heroes/pages/buscar/buscar.component.ts
+++ Angular/05-heroesApp/src/app/heroes/pages/buscar/buscar.component.ts
@@ -11,7 +11,7 @@ export class BuscarComponent implements OnInit {
 
   termino: string = '';
   heroes: Heroes[] = [];
-  heroeSeleccionado!: Heroes;
+  heroeSeleccionado: Heroes | undefined;
 
   constructor(private heroesService: HeroesService) { }
 
@@ -19,16 +19,21 @@ export class BuscarComponent implements OnInit {
   }
 
   buscando(){
-    this.heroesService.getSugerencias(this.termino)
+    this.heroesService.getSugerencias(this.termino.trim())
             .subscribe(heroes=> this.heroes = heroes)
   }
 
   opcionSeleccionada(event: MatAutocompleteSelectedEvent){
+    if(!event.option.value){
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
     const heroe: Heroes = event.option.value;
     this.termino = heroe.superhero;
 
     this.heroesService.getHeroePorId(heroe.id!)
             .subscribe(heroe=> this.heroeSeleccionado = heroe)
-  }
+    }      
+    }
 
-}
