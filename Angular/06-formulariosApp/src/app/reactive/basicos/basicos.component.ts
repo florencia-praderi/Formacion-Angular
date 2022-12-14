3e005d20cce181a8e061e25726ff29449d0eb26e
Florencia Praderi <flopraderi@gmail.com>
2022-12-14 17:08:07 +0100

    Seccion 17 - FormulariosApp
    Formularios reactivos

--- Angular/06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ Angular/06-formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //     'nombre'      : new FormControl ('RTX 4080ti'),
+  //     'precio'      : new FormControl (1500),
+  //     'existencias' : new FormControl (5)
+  // })
+
+  //FormBuilder: inyecto el servicio y lo inicio con sus propiedades
+  miFormulario: FormGroup = this.fb.group({
+      nombre      : [, [Validators.required, Validators.minLength(2)]],
+      precio      : [, [Validators.required, Validators.min(0)]],
+      existencias : [, [Validators.required, Validators.min(0)]]
+  })
+
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      nombre: 'RTZ 4080ti',
+      precio: 1600
+    })
   }
 
+  campoValido(campo: string){
+    return this.miFormulario.controls[campo].errors
+          && this.miFormulario.controls[campo].touched
+  }
+
+  guardar(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched()
+      return;
+    }
+    console.log(this.miFormulario.value)
+    this.miFormulario.reset();
+  }
 }
